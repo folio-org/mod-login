39d39ae1d3d836ffbfae2a8524a5ac5e05c9260f
Serhii Nosko <Serhii_Nosko@epam.com>
2023-03-31 13:33:33 +0300

    MODLOGIN-206. Create user_tenant table

--- src/main/resources/templates/db_scripts/consortia/create_user_tenant_table.sql
+++ src/main/resources/templates/db_scripts/consortia/create_user_tenant_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS user_tenant (
     id uuid PRIMARY KEY,
-    tenant_id text NOT NULL,
     user_id uuid NOT NULL,
     username text NOT NULL,
+    tenant_id text NOT NULL,
     creation_date timestamp without time zone
 );
 
-CREATE INDEX IF NOT EXISTS tenant_id_username_index ON user_tenant USING BTREE (tenant_id, username);
+CREATE UNIQUE INDEX IF NOT EXISTS username_tenant_id_index ON user_tenant USING BTREE (username, tenant_id);
