678dacc5ccae7de5f0f7c52fc6758d70ff44266d
Khamidulla Abdulkhakimov <Khamidulla_Abdulkhakimov@epam.com>
2023-06-08 15:23:32 +0500

    MODLOGIN-212 - Increased log level

--- src/main/java/org/folio/rest/impl/LoginAPI.java
+++ src/main/java/org/folio/rest/impl/LoginAPI.java
@@ -308,7 +308,7 @@ public class LoginAPI implements Authn {
             List<String> matchingTenants = userTenants.stream()
                 .map(UserTenant::getTenantId)
                 .collect(toList());
-            logger.debug(MULTIPLE_MATCHING_USERS_LOG, credentials.getUsername(), credentials.getUserId(),
+            logger.warn(MULTIPLE_MATCHING_USERS_LOG, credentials.getUsername(), credentials.getUserId(),
                 matchingTenants.toString());
             promise.complete(tenantId);
           } else {
